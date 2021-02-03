8d4b045cd28a51230bd18c038885d0a08c282a85
David Wright <davidjwright8@gmail.com>
2021-02-03 09:19:37 +0000

    Add client IP address to /api/whoami response

--- server.js
+++ server.js
@@ -31,3 +31,8 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+app.get("/api/whoami", (req, res) => {
+  console.log(req.ip);
+  res.json({ipadress: req.ip});
+});
