8211e9629450741ae9e2f9c497e6911ab67d9d4f
David Wright <davidjwright8@gmail.com>
2021-02-03 09:21:37 +0000

    Add middleware to log out the client's request

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
